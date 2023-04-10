8679328471b18f640b42c1618971b7258052d9de
omkundu <omkundu156@gmail.com>
2023-04-10 12:33:21 +0530

    first-commit

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const http=require("http");
 
 const server=http.createServer((req,res)=>{
-       
+      if(req.url==="/"){
+        res.end("hello");
+      } 
+      else if(req.url==="/reports"){
+        res.end("reports");
+      } 
+      else if(req.url==="/data"){
+        res.end("Data")
+      }
+})
+
+
+server.listen(8000,()=>{
+    console.log("Listening on port 8000")
 })
