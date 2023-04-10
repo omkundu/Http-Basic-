d3fd2ea95019d7b88c725f9827d8f068c3fa8207
omkundu <omkundu156@gmail.com>
2023-04-10 14:51:47 +0530

    http-fourth-commit

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const http=require("http");
-
+const fs=require("fs")
 const server=http.createServer((req,res)=>{
       if(req.url==="/"){
         res.end("hello");
       } 
-      else if(req.url==="/reports"){
-        res.end("reports");
-      } 
-      else if(req.url==="/data"){
-        res.end("Data")
-      }
+    //   else if(req.url==="/reports"){
+    //     res.end("reports");
+    //   } 
+    //   else if(req.url==="/data"){
+    //     res.end("Data")
+    //   }
+
+    else if(req.url==="/blogs"){
+        res.write("Here are some blogs")
+        res.write("Here are some first blog...")
+        res.write("Here are some second blogs...")
+        res.end()
+    }
+    else if(req.url==="/notes"){
+      fs.readFile("./lecture.txt",{encoding:"utf-8"},(err,data)=>{
+        res.end(data)
+      })
+    }
+    else{
+        res.end("Invalid API endpoints, Please refer API Docs")
+    }
 })
 
 
